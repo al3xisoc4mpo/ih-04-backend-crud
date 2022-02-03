028541ab681530f8310eff1169be4b8a6a612de8
Alexis Ocampo <al3xisoc4mpo@gmail.com>
2022-02-03 12:14:31 -0600

    Desarrollo de seeds.js

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,10 @@
 // 1. IMPORTACIONES
 const express = require("express")
 const router = express.Router()
+const indexController = require("./../controllers/indexController")
 
 // 2. RUTEO
-router.get("/", (req, res) => {
-    res.send("hola")
-})
+router.get("/",indexController.getHome)
 
 // 3. EXPORTACIÓN
 module.exports = router
